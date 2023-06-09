2293eaf0fa8bf88ec2e0dbbf16c658b3887f33b3
2847426033@qq.com <2847426033@qq.com>
2023-06-09 16:39:01 +0800

    feat: 更新0.2.7

--- src/index.js
+++ src/index.js
@@ -18,9 +18,6 @@ const targetFile = path.resolve(__dirname, '../../../package.json');
 // const packagejson = require(targetFile);
 // const currentVersion = packagejson.version;
 const packageData = require(targetFile)
-console.log('zip packageData name==========', packageData.name)
-console.log('zip packageData version==========', packageData.version)
-
 const extract = function(date) {
   // 返回 ['2022', '09', '19', '18', '06', '11', '187']
   // 返回 2023.05.06.17.13
@@ -33,12 +30,17 @@ const extract = function(date) {
 
 const time = `${extract(new Date())}`
 
-const plugin = function({ sourcePath, desPath }) {
+const plugin = function({ sourcePath, desPath } = {}) {
+  console.log('zip packageData name==========', packageData.name)
+  console.log('zip packageData version==========', packageData.version)
+  console.log('zip sourcePath==========', sourcePath)
+  console.log('zip desPath==========', desPath)
+
   const packageName = desPath || `${packageData.name}@${packageData.version}`
   const outputDirName = sourcePath || 'dist'
+  const destinationPath = `${packageName}-${time}`
 
-  console.log('zip time==========', time)
-  console.log('开始压缩==========', outputDirName, packageName)
+  console.log('zip 开始压缩==========', outputDirName, destinationPath)
   return new FileManagerPlugin({
     // events: {
     onEnd: {
